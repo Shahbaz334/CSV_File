84f261930b0307579160e18e3be05863840a1071
Shahbaz3334 <shahbax.rajput237@gmail.com>
2022-01-20 18:35:28 +0500

    clean code

--- src/CSV_Data.js
+++ src/CSV_Data.js
@@ -3,15 +3,13 @@ import { useState } from 'react';
 import logo from './cat.jpg'
 import cross from './cross.png';
 import { CSVLink } from "react-csv";
-import {  Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 function Post() {
     const [show, setShow] = useState(false);
     const [FirstName, setFirstName] = useState('')
     const [LastName, setLastName] = useState('')
     const [Email, setEmail] = useState('')
     const [Age, setAge] = useState('')
-
     const [isUpdate, setIsUpdate] = useState(false)
     const [id, setId] = useState(-2)
     const [data, setData] = useState([])
@@ -20,29 +18,28 @@ function Post() {
         { label: "Last Name", key: "lastName" },
         { label: "Email", key: "email" },
         { label: "Age", key: "age" }
-      ];
-      const csvReport = {
+    ];
+    const csvReport = {
         data: data,
         headers: headers,
         filename: 'Report.csv'
-      };
+    };
     const createData = (obj) => {
-        if(id){
+        if (id) {
             const temp = data;
             const index = data.findIndex(item => item.id == id)
             temp[index] = { ...temp[index], ...obj }
             setData(temp)
             setIsUpdate(false)
             setId('')
-
-        }else{
+        } 
+        else {
             setData([obj, ...data])
             setFirstName("")
             setLastName("")
             setEmail("")
             setAge("")
         }
-       
     }
     const updateData = (obj) => {
         setFirstName(obj.FirstName)
@@ -52,14 +49,10 @@ function Post() {
         setId(obj.id)
         setIsUpdate(true)
     }
-
     const deleteData = (id) => {
         setData(data.filter(item => item.id != id))
     }
-
-
     return (
-
         <div>
             <div className="container" style={{ borderWidth: 5, borderColor: 'black' }}>
                 <div className="row">
@@ -69,10 +62,8 @@ function Post() {
                                 <img src={logo} />
                             </div>
                             <div className="text" onClick={() => setShow(true)}>
-
                                 <input type="text" placeholder="What's on your mind" />
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -119,22 +110,22 @@ function Post() {
                             </div>
                             <div className='Button'>
                                 <button
-                                    className="btn btn-primary" onClick={() =>createData({
-                                        firstName: FirstName ,
-                                      lastName:LastName,
-                                        email:Email,
-                                        age:Age,
+                                    className="btn btn-primary" onClick={() => createData({
+                                        firstName: FirstName,
+                                        lastName: LastName,
+                                        email: Email,
+                                        age: Age,
                                         id: Math.random()
-                                    })}>{isUpdate ? "Update": "Submit"}</button>
+                                    })}>{isUpdate ? "Update" : "Submit"}</button>
                             </div>
                         </div>
                     </div>
                     : null}
             </div>
-            <Link style={{marginLeft:'20%'}} to="/import">ImportCSV</Link>
-            <span style={{marginLeft:'50%',borderWidth:2,borderRadius:30}}>
-      <CSVLink {...csvReport}>Export to CSV</CSVLink>
-      </span>
+            <Link style={{ marginLeft: '20%' }} to="/import">ImportCSV</Link>
+            <span style={{ marginLeft: '50%', borderWidth: 2, borderRadius: 30 }}>
+                <CSVLink {...csvReport}>Export to CSV</CSVLink>
+            </span>
             <table>
                 <tbody>
                     {data?.map((item, index) => (
@@ -151,7 +142,6 @@ function Post() {
                             </td>
                         </tr>
                     ))}
-
                 </tbody>
             </table>
         </div>
--- src/ImportData.js
+++ src/ImportData.js
@@ -2,57 +2,44 @@
 import React, { useState } from 'react';
 import * as XLSX from 'xlsx';
 import DataTable from 'react-data-table-component';
-
-
-
-
-
-
 function ImportData() {
-
-    const [columns, setColumns] = useState([]);
-const [data, setData] = useState([]);
-
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
   // process CSV data
   const processData = dataString => {
     const dataStringLines = dataString.split(/\r\n|\n/);
     const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
- 
     const list = [];
     for (let i = 1; i < dataStringLines.length; i++) {
       const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
       if (headers && row.length == headers.length) {
         const obj = {};
         for (let j = 0; j < headers.length; j++) {
-          let d = row[j];
-          if (d.length > 0) {
-            if (d[0] == '"')
-              d = d.substring(1, d.length - 1);
-            if (d[d.length - 1] == '"')
-              d = d.substring(d.length - 2, 1);
+          let CSV_Data = row[j];
+          if (CSV_Data.length > 0) {
+            if (CSV_Data[0] == '"')
+              CSV_Data = CSV_Data.substring(1, CSV_Data.length - 1);
+            if (CSV_Data[CSV_Data.length - 1] == '"')
+              CSV_Data = CSV_Data.substring(CSV_Data.length - 2, 1);
           }
           if (headers[j]) {
-            obj[headers[j]] = d;
+            obj[headers[j]] = CSV_Data;
           }
         }
- 
         // remove the blank rows
         if (Object.values(obj).filter(x => x).length > 0) {
           list.push(obj);
         }
       }
     }
- 
     // prepare columns list from headers
     const columns = headers.map(c => ({
       name: c,
       selector: c,
     }));
- 
     setData(list);
     setColumns(columns);
   }
- 
   // handle file upload
   const handleFileUpload = e => {
     const file = e.target.files[0];
@@ -70,25 +57,20 @@ const [data, setData] = useState([]);
     };
     reader.readAsBinaryString(file);
   }
- 
-    return (
-
-       
-        
-          <div>
-            <input
-              type="file"
-              accept=".csv,.xlsx,.xls"
-              onChange={handleFileUpload}
-            />
-            <DataTable
-              pagination
-              highlightOnHover
-              columns={columns}
-              data={data}
-            />
-          </div>
-        );
-      }
-
+  return (
+    <div>
+      <input
+        type="file"
+        accept=".csv,.xlsx,.xls"
+        onChange={handleFileUpload}
+      />
+      <DataTable
+        pagination
+        highlightOnHover
+        columns={columns}
+        data={data}
+      />
+    </div>
+  );
+}
 export default ImportData
